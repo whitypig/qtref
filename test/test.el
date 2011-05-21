074fd54e2816cdf43c6702b27557c5f7ea610b9a
whitypig <whitypig@gmail.com>
2011-05-21 17:39:08 +0900

    Removed test-related files.

--- test/test.el
+++
@@ -1,9 +0,0 @@
-(require 'el-expectations)
-
-
-(expectations
-  (expect t
-    (let ((alist (qtref-build-classname-mapping-alist "./testdata.html")))
-      (and 
-       (string= "qabstractanimation.html" (cdr (assoc "QAbstractAnimation" alist)))
-       (string= "qabstractbutton.html" (cdr (assoc "QAbstractButton" alist)))))))
